4977a232a3438f58113056b86ccb027f646b42f7
rhekord <rhekord1@gmail.com>
2025-03-12 10:47:19 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+       <div className = "pattern">
+
+        <div className = "Wrapper">
+
+          <hearder>
+            <img src="./hero.png" alt="Hero Banner"/>
+            <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          </hearder>
+            
+            <p> Search</p>
+        </div>
+
+
+       </div>
+    </main>
+
+  )
+}
+
+export default App
